bf2ca7e01cfaa6763f7d53b01436e09eecf65765
dalinwanglhtz <dalinwanglhtz@gmail.com>
2022-03-20 14:55:53 +1100

    Update home page interactions

--- force-app/main/default/lwc/todolist/todolist.js
+++ force-app/main/default/lwc/todolist/todolist.js
@@ -1,6 +1,31 @@
 import { LightningElement } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import getPopReminders from '@salesforce/apex/PopReminderController.getPopReminders';
+import getUser from '@salesforce/apex/PopReminderController.getUser';
+import getPopUser from '@salesforce/apex/PopReminderController.getPopUser';
 
 export default class Todolist extends LightningElement {
+    isPopUser;
+    popUser;
+    userName;
 
+    connectedCallback() {
+        getPopUser()
+        .then(result => {
+            console.log('Success: ', result.Persona__c);
+            this.isPopUser = true;
+            getUser({userId: result.Persona__c})
+            .then(output => {
+                console.log('User Name: ', output);
+                this.userName = output.Name;
+            })
+            .catch(err => {
+                console.log('User error: ', err.message);
+            });
+        })
+        .catch(error => {
+            console.log('Error: ',error.message);
+            this.isPopUser = false;
+        });
+    }
 }
